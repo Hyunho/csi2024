75a838dead4e441e48bbc3b920825492d4f29764
hyunho <oksknoh@gmail.com>
2023-12-02 21:30:33 +0900

    upgrade nextjs 13 to 14

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,7 @@ import typographyStyles from './typography'
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
-  plugins: [typographyPlugin],
+  plugins: [typographyPlugin, require("tailwindcss-animate")],
   theme: {
     fontSize: {
       xs: ['0.8125rem', { lineHeight: '1.5rem' }],
