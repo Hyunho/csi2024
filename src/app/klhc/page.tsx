28a09336e66711220d7aa407f1071ac909fea8ee
hyunho <hyunho@telcoware.com>
2023-12-14 01:10:15 +0900

    update klhc page

--- src/app/klhc/page.tsx
+++ src/app/klhc/page.tsx
@@ -42,37 +42,89 @@ export const metadata = {
 export default function Uses() {
   return (
     <SimpleLayout
-      title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      title="Korean Lindy Hop Championships"
+      intro="Korean Lindy Hop Championships (KLHC) is Korea’s largest international competition where the best lindyhoppers for each division are selected. We sincerely hope that KLHC does not stop at just being a place to win simple results; we want it to be a stage of challenge and opportunity. Be there, compete, test your limits."
     >
       <div className="space-y-20">
-        <ToolsSection title="Routine">
-          <Tool title="Classic Lindy Hop">
-            I was using an Intel-based 16” MacBook Pro prior to this and the
-            difference is night and day. I’ve never heard the fans turn on a
-            single time, even under the incredibly heavy loads I put it through
-            with our various launch simulations.
+        <h1>Divisions</h1>
+        <ToolsSection title="Routine Divisions">
+          <Tool title="The Lindy Hop World Cup Showcase (couples division)">
+            <p>
+              A two-to-four minute long routine, self-selected music. No particular restrictions as long as it is lindy hop. Lifts, drops, aerials, air steps, acrobatics all allowed.
+            </p>
+            <p>
+              자체 선곡한 2-4분 길이의 루틴으로, 린디합이라면 어떠한 제약도 없음. 리프트, 드롭, 에어리얼, 에어스텝, 아크로바틱 등이 허용됨.
+            </p>
           </Tool>
-          <Tool title="Lindy Hop Team">
-            The only display on the market if you want something HiDPI and
-            bigger than 27”. When you’re working at planetary scale, every pixel
-            you can get counts.
+          <Tool title="Lindy Hop Classic (non-showcase: couples division)">
+            <p>
+              A two-to-four minute long routine, self-selected music. No lifts, aerials or air steps
+            </p>
+            <p>
+              자체 선곡한 2-4분 길이의 루틴으로, 리프트, 에어리얼, 에어스텝은 허용되지 않음.
+            </p>
+          </Tool>
+          <Tool title="Team Division">
+            <p>
+              A three-to-four minute long routine, self-selected music. In teams of more than 3 couples.
+            </p>
+            <p>
+              자체 선곡한 3-4분 길이의 루틴으로, 3쌍 이상의 커플로 이루어진 팀.
+            </p>
           </Tool>
         </ToolsSection>
-        <ToolsSection title="Improvisation">
+        <ToolsSection title="Improvisation Divisions">
           <Tool title="Mix & Match">
-            I don’t care if it’s missing all of the fancy IDE features everyone
-            else relies on, Sublime Text is still the best text editor ever
-            made.
+            <ul>
+              <li>Rookie (Under 2 years experience)</li>
+              <li>Open</li>
+              <li>Advanced</li>
+              <li>All Star
+                  * Qualifications (winner refers to 1st, 2nd and/or 3rd places)
+                <ol>
+                  <li>
+                  1) Winners at KLHC Advanced M&M
+                  </li>
+                  <li>
+2) Winners at Summer Swing Dance Championships @ Busan Swing Festival Advanced M&M
+                  </li>
+                  <li>
+3) Finalists at The Battle Korea Advanced M&M (1st or 2nd), winners at All-star M&M (quarter finalists)
+                  </li>
+                  <li>
+4) Otherwise deemed qualified by KLHC organizers 
+                  </li>
+
+                </ol>
+
+              </li>
+            </ul>
           </Tool>
-          <Tool title="Strictly">
-            I’m honestly not even sure what features I get with this that aren’t
-            just part of the macOS Terminal but it’s what I use.
+          <Tool title="Strictly Lindy">
+            <ul>
+              <li>Open</li>
+              <li>Advanced</li>
+              <li>
+                All Star
+                * Qualifications
+                <ol>
+                  <li>
+                    1) Winners at KLHC Showcase, Classic and Advanced Strictly divisions
+                  </li>
+                  <li>
+                    2) Winners at Korea Open Swing Championships hosted by Kukmin University – Showcase
+                  </li>
+                  <li>
+                    3) Finalists at The Battle Korea Advanced Strictly (1st or 2nd), winners at All-star Strictly (quarter finalists)
+                  </li>
+                  <li>
+                    4) Otherwise deemed qualified by KLHC organizers
+                  </li>
+                </ol>
+              </li>
+            </ul>
           </Tool>
           <Tool title="Solo Jazz">
-            The Solo Jazz category is a jazz dance competition.
-            Dancers should include Charleston and Jazz movements and dance to music selected by the competition.
-            The finals will be held in spotlight format with a live band. In the case of judging, it is not forbidden to create a routine, but the judging process will focus on improvisation, creativity and relevance to music.
           </Tool>
         </ToolsSection>
       </div>
