8fcfd69a3664904f1a286ccf7661a803349d2619
hyunho <hyunho@telcoware.com>
2024-01-09 18:08:55 +0900

    add images for veneus

--- src/app/venues/page.tsx
+++ src/app/venues/page.tsx
@@ -54,6 +54,123 @@ export const metadata: Metadata = {
     'I’m Spencer Sharp. I live in New York City, where I design the future.',
 }
 
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+const features = [
+  {
+    name: '효성문화홀',
+    description: 'Today, Next, and Someday cards allow you to defer your dreams into the future.',
+    imageSrc: '/images/venues/culture_hall.jpeg',
+    imageAlt: 'Green cardstock box containing white, beige, and brown cards.',
+  },
+  {
+    name: '오디토리움',
+    description: 'Each refill pack contains plenty of cards to last you a month of procrastination.',
+    imageSrc: '/images/venues/auditorium.jpeg',
+    imageAlt: 'Green cardstock box open with 50 cards inside.',
+  },
+  {
+    name: '효정국제컨퍼런스홀',
+    description: 'Flip a card over to doodle during meetings when you should be listening.',
+    imageSrc: '/images/venues/conference_hall1.jpeg',
+    imageAlt: 'Detail of white today card, beige next card, and brown someday card with dot grid.',
+  },
+  {
+    name: '효정국제컨퍼런스홀',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/conference_hall2.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '인포메이션 데스크',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/information.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '식당',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/restaurants.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '숙소동 전경',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/lodging1.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '숙소동',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/lodging2.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '숙소동',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/lodging3.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '효정문화카페',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/cafe1.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+  {
+    name: '효정문화카페',
+    description: 'Subscribe and save on routine refill packs to keep you productive all year long.',
+    imageSrc: '/images/venues/cafe2.jpeg',
+    imageAlt: 'Stack of three green cardstock boxes with 3 hole cutouts showing cards inside.',
+  },
+]
+
+function Pictures() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-2xl px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8">
+        <div className="max-w-3xl">
+          <h2 id="features-heading" className="font-medium text-gray-500">
+            Pictures
+          </h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Simple productivity</p>
+          <p className="mt-4 text-gray-500">
+            Focus allows you to plan 10 daily tasks, while also thinking ahead about what's next. Forget distracting
+            digital apps and embrace these small, sturdy pieces of paper.
+          </p>
+        </div>
+
+        <div className="mt-11 grid grid-cols-1 items-start gap-x-6 gap-y-16 sm:mt-16 sm:grid-cols-2 lg:grid-cols-4 lg:gap-x-8">
+          {features.map((feature) => (
+            <div key={feature.name} className="flex flex-col-reverse">
+              <div className="mt-6">
+                <h3 className="text-sm font-medium text-gray-900">{feature.name}</h3>
+                {/* <p className="mt-2 text-sm text-gray-500">{feature.description}</p> */}
+              </div>
+              <div className="aspect-h-1 aspect-w-1 overflow-hidden rounded-lg bg-gray-100">
+                <img src={feature.imageSrc} alt={feature.imageAlt} className="object-cover object-center" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
 export default function About() {
   return (
     <Container className="mt-20 sm:mt-32">
@@ -123,7 +240,9 @@ export default function About() {
             </p>
           </div>
         </div> */}
+        
       </div>
+      <Pictures/>
     </Container>
   )
 }
