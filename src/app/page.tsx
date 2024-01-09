d53f174fc1760c153b5e0d57c59669ab4a9c6dca
hyunho <hyunho@telcoware.com>
2024-01-09 10:49:04 +0900

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -168,11 +168,11 @@ const instructor_people= [
 function Instructors() {
   return (
     <div className="bg-white py-8 sm:py-12">
+      <div className="mx-auto max-w-2xl lg:mx-0">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Dancers</h2>
+          The 2024 artists line up of Camp Swing It is as follows.
+      </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Dancers</h2>
-            The 2024 artists line up of Camp Swing It is as follows.
-        </div>
         <ul
           role="list"
           className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none"
