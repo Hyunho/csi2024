12af9f99232605dd540142790bad12f936a5fc4c
hyunho <hyunho@telcoware.com>
2024-01-09 13:28:06 +0900

    add total band image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -225,6 +225,20 @@ function Band() {
   return (
   <div className="bg-white py-8 md:py-12">
       <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Musicions</h2>
+      <div className="relative overflow-hidden pt-16">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <img
+            src="/images/2024/total_band.jpeg"
+            alt="App screenshot"
+            className="rounded-xl shadow-2xl ring-1 ring-gray-900/10"
+            width={2432}
+            height={1442}
+          />
+          <div className="relative" aria-hidden="true">
+            <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]" />
+          </div>
+        </div>
+      </div>
       <div>
         {/* <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Musicions</h2> */}
         <p className="mt-6 text-lg leading-8 text-gray-600">
@@ -234,20 +248,7 @@ function Band() {
           Shirt Tail Stompers와 Adamas kim and the Rhythm shufflers와 함께하는 최고의 재즈 퓨전을 경험하세요! 할렘 스윙과 초기 재즈의 조화로 밤새도록 짜릿한 리듬을 즐길 수 있습니다.
         </p>
       </div>
-      <div className="relative overflow-hidden pt-16">
-        <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <img
-            src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
-            alt="App screenshot"
-            className="mb-[-12%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
-            width={2432}
-            height={1442}
-          />
-          <div className="relative" aria-hidden="true">
-            <div className="absolute -inset-x-20 bottom-0 bg-gradient-to-t from-white pt-[7%]" />
-          </div>
-        </div>
-      </div>
+   
       <div>
         <p>
         </p>
