1a9817a02a0e963bf1c6b3dd85baddc6a4727b93
hyunho <hyunho@telcoware.com>
2024-11-27 18:46:52 +0900

    csi2025

--- src/app/klhc/KlhcTable.tsx
+++ src/app/klhc/KlhcTable.tsx
@@ -6,31 +6,48 @@ export function KlhcTable() {
         <table>
         <thead>
           <tr>
+            <th>Divisions</th>
+            <th>APR 4 (THU)</th>
             <th>APR 5 (FRI)</th>
             <th>APR 6 (SAT)</th>
-            <th>APR 7 (SUN)</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>Strictly Open, Adv. All-star</td>
-            <td>Lindy Hop Couple</td>
-            <td>Awards</td>
-          </tr>
-          <tr>
-            <td>Strictly Advanced</td>
+            <td>Routine</td>
+            <td></td>
             <td>Team</td>
-            {/* <td>Stone Cold Stunner</td> */}
+            <td>Lindy Hop Couple</td>
           </tr>
           <tr>
-            <td>Strictly All-Star</td>
-            <td>M&M Rookie, Open, Adv. All-star</td>
-            {/* <td>Elbow Drop</td> */}
+            <td>Balboa</td>
+            <td>Balboa M&M Open</td>
+            <td>Balboa M&M Amateur</td>
+            <td>Balboa M&M Rookie</td>
+          </tr>
+          <tr>
+            <td>Improvisation</td>
+            <td>Strictly Open</td>
+            <td>M&M Open</td>
+            <td>M&M Rookie</td>
           </tr>
           <tr>
             <td></td>
+            <td>Strictly Advanced</td>
+            <td>M&M Advanced</td>
             <td>Solo</td>
+          </tr>
+          <tr>
             <td></td>
+            <td>Strictly All-Star</td>
+            <td>M&M All-star</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Awards</td>
+            <td></td>
+            <td></td>
+            <td>Awards</td>
           </tr>
         </tbody>
       </table>
