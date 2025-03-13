14e1aafe1137afa4134c51b44e58da918a4d9f9c
hyunho <hyunho@telcoware.com>
2025-03-13 17:54:34 +0900

    apply tailwind to schedule page

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -4,26 +4,10 @@ export default function Schedule() {
     return (
         <Container>
             <h1>Schedule</h1>
-            <div style={{
-                position: 'fixed',
-                top: '50px',
-                left: 0,
-                right: 0,
-                bottom: 0,
-                width: '100vw',
-                height: '100vh',
-                overflow: 'hidden'
-            }}>
+            <div className="fixed top-16 left-0 right-0 bottom-0 w-screen h-[calc(100vh-64px)] overflow-hidden">
                 <iframe 
                     src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTR96ZN_1oXH8XNX05Z_TkUKawTjkKl5KMtN9Wwf0khXIqwa65K9Nk3UhK-dE_XA9SpyEJwyoPJN6Iy/pubhtml?gid=720224361&amp;single=true&amp;widget=true&amp;headers=false"
-                    style={{
-                        width: '100%',
-                        height: '100%',
-                        border: 'none',
-                        position: 'absolute',
-                        top: 0,
-                        left: 0
-                    }}
+                    className="w-full h-full border-none absolute inset-0"
                 />
             </div>
             <div>
