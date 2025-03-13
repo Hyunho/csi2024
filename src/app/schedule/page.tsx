b41a9a5260935254181d9a43c6853904b925b8a9
hyunho <hyunho@telcoware.com>
2025-03-13 18:15:30 +0900

    update schedule link

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -5,8 +5,8 @@ export default function Schedule() {
         <Container>
             <h1>Schedule</h1>
             <div className="fixed top-16 left-0 right-0 bottom-0 w-screen h-[calc(100vh-64px)] overflow-hidden">
-                <iframe 
-                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTR96ZN_1oXH8XNX05Z_TkUKawTjkKl5KMtN9Wwf0khXIqwa65K9Nk3UhK-dE_XA9SpyEJwyoPJN6Iy/pubhtml?gid=720224361&amp;single=true&amp;widget=true&amp;headers=false"
+                <iframe
+                    src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSHnsk8Et01HHrkvQ7hvEFvnlYLQZyiRjp6sYLKs2t9pw5lLoQTlwkidC-cdaDR_4GYoK76AIONq_7l/pubhtml?gid=720224361&amp;single=true&amp;widget=true&amp;headers=false"
                     className="w-full h-full border-none absolute inset-0"
                 />
             </div>
