96d3f38a0eb08837b8045100b2db45133fad728e
hyunho <hyunho@telcoware.com>
2025-03-13 18:13:56 +0900

    Revert "스케쥴 시트를 모바일 화면에 맞추도록 합니다."
    This reverts commit 9fb4cd192efa8c52f21c3622fd6dd02be6ea7231.

--- src/app/schedule/page.tsx
+++ src/app/schedule/page.tsx
@@ -4,10 +4,10 @@ export default function Schedule() {
     return (
         <Container>
             <h1>Schedule</h1>
-            <div className="w-full h-screen md:fixed md:left-0 md:right-0 md:bottom-0 md:w-screen overflow-hidden">
+            <div className="fixed top-16 left-0 right-0 bottom-0 w-screen h-[calc(100vh-64px)] overflow-hidden">
                 <iframe 
                     src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTR96ZN_1oXH8XNX05Z_TkUKawTjkKl5KMtN9Wwf0khXIqwa65K9Nk3UhK-dE_XA9SpyEJwyoPJN6Iy/pubhtml?gid=720224361&amp;single=true&amp;widget=true&amp;headers=false"
-                    className="w-full h-full border-none"
+                    className="w-full h-full border-none absolute inset-0"
                 />
             </div>
             <div>
